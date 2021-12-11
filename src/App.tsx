c29d25a1d6a848248bdb098f825eccf2cebe11fb
s-kawabe <s.kawabe2281@gmail.com>
2021-12-11 23:33:45 +0900

    chore: fix directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { VFC } from "react";
+import { PostPage } from "@/pages/PostPage"
 
-function App() {
+export const App: VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PostPage />
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,10 @@
+import type { VFC } from 'react'
+import Button from '@mui/material/Button'
+
+export const PostPage: VFC = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  )
+}
